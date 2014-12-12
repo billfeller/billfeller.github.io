f34a383a363736f7e4c376a6535e0b4603d5e5f8
billfeller <billfeller@gmail.com>
2014-12-12 10:58:57 +0800

    缓存策略优化：1.在cache失效时只透传一定比例的请求到后端系统；2. 通过锁保证高并发情况下只透传一次请求到后端系统（需要多几个锁的操作）；

--- cache.php
+++ cache.php
@@ -3,17 +3,22 @@
 class Utils {
     const CACHED_STRATEGY_FACETOR_PRIFIX = 'csf';
 
-    public static function getCSTkey ($key, $factor) {
+    public static function getCSTKey($key, $factor) {
         return self::CACHED_STRATEGY_FACETOR_PRIFIX . '_' . $key . '_' . $factor;
     }
 
+    public static function getCSTLockKey($key, $factor) {
+        return self::CACHED_STRATEGY_FACETOR_PRIFIX . '_lock_' . '_' . $key . '_' . $factor;
+    }
+
     /**
+     * cache失效时只透传一定比例的请求到后端子系统
      * @param $method string 方法名
      * @param $params array  参数列表
      * @param $key    string 缓存key
      */
-    public static function cacheStrategy ($method, $params, $key, $factor = 10, $expire = 100) {
-        $csfKey = self::getCSTkey($key, $factor);
+    public static function cacheStrategy($method, $params, $key, $factor = 10, $expire = 100) {
+        $csfKey = self::getCSTKey($key, $factor);
         $memcached = new Memcached();
         $memcached->addServer('127.0.0.1', 11211);
         $value = $memcached->get($csfKey);
@@ -31,9 +36,9 @@ class Utils {
 
         // cache已过期或者cache在(start + expire, +inf)，透传比例为factor的请求到后台实时请求并更新cache
         $data = self::$method($params);
-        // todo: 只缓存请求成功的数据 需要过滤掉失败的情况
-        if (isset($data['requestSucc']) && $data['requestSucc'] == 0) {
-            unset($data['requestSucc']);
+        // 缓存请求成功的数据
+        if (isset($data['resultCode']) && $data['resultCode'] == 0) {
+            unset($data['resultCode']);
             $doubleExpire = time() + 2 * $expire;
             $value = array(
                 'data' => $data,
@@ -44,24 +49,75 @@ class Utils {
         return $data;
     }
 
-    public static function getProductDetail($params) {
-        if (!is_array($params)) {
-            $params = array($params);
+    /**
+     * 通过加锁机制在cache失效时并发时只透传一个请求到后端子系统
+     * @param $method string 方法名
+     * @param $params array  参数列表
+     * @param $key    string 缓存key
+     */
+    public static function cacheStrategyOnce($method, $params, $key, $expire = 100) {
+        // 做法相似，只是需要把概率判断的算法换成使用锁保证高并发请求时严格只有一个请求透传到后端系统
+        $csfKey = self::getCSTKey($key, 1);
+        $lockKey = self::getCSTLockKey($key, 1);
+
+        $memcached = new Memcached();
+        $memcached->addServer('127.0.0.1', 11211);
+        $value = $memcached->get($csfKey);
+        if (!empty($value) && !empty($value['data']) && !empty($value['doubleExpire'])) {
+            $doubleExpire = $value['doubleExpire'];
+            if (time() <= $doubleExpire - $expire) { // cache还在[start, start + expire]时间内仍有效，无需请求后端
+                return $value['data'];
+            }
+
+            $isLock = $memcached->get($lockKey);
+            if (!empty($lockKey)) { // 有锁,表示当前已有请求到后端去读取最新数据并更新cache；
+                return $value['data'];
+            }
         }
+
+        // cache已过期或者无锁，到后台实时请求并更新cache
+        $ret = $memcached->set($lockKey, 1, 1);
+        $data = self::$method($params);
+        if (isset($data['resultCode']) && $data['resultCode'] == 0) {
+            unset($data['resultCode']);
+            $doubleExpire = time() + 2 * $expire;
+            $value = array(
+                'data' => $data,
+                'doubleExpire' => $doubleExpire,
+            );
+            $ret = $memcached->set($csfKey, $value, $doubleExpire);
+        }
+        $ret = $memcached->delete($lockKey, 1);
+        return $data;
+
+    }
+
+    public static function getProductDetail($params) {
+        // TODO: 参数处理
 
         // todo : 请求后端接口
         // $data = array();
         // if (isset($data['ret']) && $data['ret'] == 0) {
-        //     $data['requestSucc'] = 0;
+        //     $data['resultCode'] = 0;
         // } else {
-        //     $data['requestSucc'] = -1;
+        //     $data['resultCode'] = -1;
         // }
 
         return array(
-            'requestSucc' => 0,
+            'resultCode' => 0,
             'product_name' => '测试',
         );
     }
 }
 
+// 测试用例
+$method = 'getProductDetail';
+$params = array(
+    'productId' => 10001,
+);
+$key = 'pid_10001';
 
+$data = Utils::cacheStrategy($method, $params, $key);
+var_dump($data);
+
+// Utils::cacheStrategyOnce($method, $params, $key);
