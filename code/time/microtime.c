5628a9ef1449af8f6770ff2ca124b1f55280dbd6
billfeller <billfeller@gmail.com>
2014-12-26 00:30:50 +0800

    microtime

---
+++ code/time/microtime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+/* Return the UNIX time in microseconds */
+long long ustime(void) {
+    struct timeval tv;
+    long long ust;
+
+    gettimeofday(&tv, NULL);
+    ust = ((long long)tv.tv_sec)*1000000;
+    ust += tv.tv_usec;
+    return ust;
+}
+
+/* Return the UNIX time in milliseconds */
+long long mstime(void) {
+    return ustime()/1000;
+}
+
+int main() {
+    printf("microseconds: %lld\n", ustime());
+    printf("milliseconds: %ld\n", mstime());
+    return 0;
+}
