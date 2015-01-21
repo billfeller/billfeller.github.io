34545e0e7ddb4f977443a4d3c55c8a367a112eec
billfeller <billfeller@gmail.com>
2015-01-21 23:40:45 +0800

    随机生成32位字符串

--- code/alg/testRand32Str.php
+++ code/alg/testRand32Str.php
@@ -1,4 +1,15 @@
 <?php
+// vim: set expandtab cindent tabstop=4 shiftwidth=4 fdm=marker:
+ 
+/**
+ * @file     testRand32Str.php
+ * @version  1.0
+ * @author   wade
+ * @date     2015-01-21 23:38:17
+ */
+
+// 测试相应性能数据
+// time /usr/local/php/bin/php testRand32Str.php
 
 function rand32Str($len = 32) {
     $str = '0123456789abcdef';
