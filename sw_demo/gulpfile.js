ae0f443e2ed21c2b8d1169ed3bcd6280e87af986
billfeller <billfeller@gmail.com>
2017-01-04 10:20:42 +0800

    service worker

---
+++ sw_demo/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var swPrecache = require('sw-precache');
+var connect = require('gulp-connect');
+
+var rootDir = 'dist/';
+
+/*************** generate service worker ******************/
+gulp.task('gsw', function(callback) {
+    swPrecache.write(`${rootDir}/service-worker.js`, {
+        staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+        stripPrefix: rootDir,
+        runtimeCaching: [{
+            urlPattern: /^https:\/\/pc[1-4]\.gtimg\.com\//,                             // cdn
+            handler: 'fastest',
+            options: {
+                cache: {
+                    name: 'cdn'
+                }
+            }
+        }]
+    }, callback);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist/',
+    index : 'index.html',
+    port : 80,
+    livereload: true
+  });
+});
