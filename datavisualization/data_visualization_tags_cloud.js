8ea9540af6888984d24acea8330d23339ca3e197
billfeller <billfeller@users.noreply.github.com>
2018-04-23 21:52:58 +0800

    Create data_visualization_tags_cloud.js

---
+++ datavisualization/data_visualization_tags_cloud.js
@@ -0,0 +1,443 @@
+option = {
+    title: {
+        text: "github/tags/data-visualization 可视化",
+        link: 'https://github.com/topics/data-visualization'
+    },
+    tooltip: {},
+    series: [{
+        type: 'wordCloud',
+        gridSize: 20,
+        sizeRange: [12, 50],
+        rotationRange: [0, 0],
+        shape: 'circle',
+        textStyle: {
+            normal: {
+                color: function () {
+                    return 'rgb(' + [
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160)
+                    ].join(',') + ')';
+                }
+            },
+            emphasis: {
+                shadowBlur: 10,
+                shadowColor: '#333'
+            }
+        },
+        data: [
+            {
+                "name": "incubator-echarts",
+                "value": 27000
+            },
+            {
+                "name": "incubator-superset",
+                "value": 18000
+            },
+            {
+                "name": "metabase",
+                "value": 9000
+            },
+            {
+                "name": "plotly.js",
+                "value": 8000
+            },
+            {
+                "name": "awesome-datascience",
+                "value": 7000
+            },
+            {
+                "name": "c3",
+                "value": 7000
+            },
+            {
+                "name": "vis",
+                "value": 6000
+            },
+            {
+                "name": "awesome-bigdata",
+                "value": 6000
+            },
+            {
+                "name": "victory",
+                "value": 5000
+            },
+            {
+                "name": "OpenRefine",
+                "value": 5000
+            },
+            {
+                "name": "raw",
+                "value": 5000
+            },
+            {
+                "name": "g2",
+                "value": 4000
+            },
+            {
+                "name": "dash",
+                "value": 4000
+            },
+            {
+                "name": "deck.gl",
+                "value": 4000
+            },
+            {
+                "name": "heatmap.js",
+                "value": 4000
+            },
+            {
+                "name": "facets",
+                "value": 4000
+            },
+            {
+                "name": "vx",
+                "value": 3000
+            },
+            {
+                "name": "react-map-gl",
+                "value": 3000
+            },
+            {
+                "name": "awesome-creative-coding",
+                "value": 3000
+            },
+            {
+                "name": "tui.chart",
+                "value": 3000
+            },
+            {
+                "name": "react-vis",
+                "value": 3000
+            },
+            {
+                "name": "britecharts",
+                "value": 3000
+            },
+            {
+                "name": "billboard.js",
+                "value": 2000
+            },
+            {
+                "name": "Live-Charts",
+                "value": 2000
+            },
+            {
+                "name": "react-trend",
+                "value": 1000
+            },
+            {
+                "name": "sqlpad",
+                "value": 1000
+            },
+            {
+                "name": "calendar-heatmap",
+                "value": 1000
+            },
+            {
+                "name": "awesome-dataviz",
+                "value": 1000
+            },
+            {
+                "name": "inmap",
+                "value": 1000
+            },
+            {
+                "name": "just-dashboard",
+                "value": 1000
+            },
+            {
+                "name": "orange3",
+                "value": 1000
+            },
+            {
+                "name": "DAT8",
+                "value": 1000
+            },
+            {
+                "name": "JSCity",
+                "value": 1000
+            },
+            {
+                "name": "plotly",
+                "value": 1000
+            },
+            {
+                "name": "luma.gl",
+                "value": 995
+            },
+            {
+                "name": "CBoard",
+                "value": 974
+            },
+            {
+                "name": "Dex",
+                "value": 910
+            },
+            {
+                "name": "victory-native",
+                "value": 877
+            },
+            {
+                "name": "D3-ID3",
+                "value": 790
+            },
+            {
+                "name": "d4",
+                "value": 754
+            },
+            {
+                "name": "sourcerer-app",
+                "value": 735
+            },
+            {
+                "name": "AAChartKit-Swift",
+                "value": 687
+            },
+            {
+                "name": "explorer",
+                "value": 680
+            },
+            {
+                "name": "chart",
+                "value": 666
+            },
+            {
+                "name": "crayon",
+                "value": 664
+            },
+            {
+                "name": "wesanderson",
+                "value": 622
+            },
+            {
+                "name": "visdown",
+                "value": 597
+            },
+            {
+                "name": "party-mode",
+                "value": 592
+            },
+            {
+                "name": "pbpython",
+                "value": 586
+            },
+            {
+                "name": "awesome-clojurescript",
+                "value": 585
+            },
+            {
+                "name": "electricitymap",
+                "value": 563
+            },
+            {
+                "name": "keen-js",
+                "value": 562
+            },
+            {
+                "name": "leaflet-dvf",
+                "value": 535
+            },
+            {
+                "name": "hypertools",
+                "value": 491
+            },
+            {
+                "name": "d3-node",
+                "value": 412
+            },
+            {
+                "name": "data-science-with-ruby",
+                "value": 398
+            },
+            {
+                "name": "jsxgraph",
+                "value": 382
+            },
+            {
+                "name": "hrbrthemes",
+                "value": 364
+            },
+            {
+                "name": "tabix",
+                "value": 364
+            },
+            {
+                "name": "engsoccerdata",
+                "value": 322
+            },
+            {
+                "name": "worldview",
+                "value": 306
+            },
+            {
+                "name": "waffle",
+                "value": 300
+            },
+            {
+                "name": "Book-SocialMediaMiningPython",
+                "value": 275
+            },
+            {
+                "name": "jfreechart",
+                "value": 265
+            },
+            {
+                "name": "victory-chart",
+                "value": 253
+            },
+            {
+                "name": "jSchema",
+                "value": 241
+            },
+            {
+                "name": "ggpage",
+                "value": 236
+            },
+            {
+                "name": "overwatch",
+                "value": 223
+            },
+            {
+                "name": "dropchop",
+                "value": 213
+            },
+            {
+                "name": "fun",
+                "value": 212
+            },
+            {
+                "name": "rthreejs",
+                "value": 200
+            },
+            {
+                "name": "elm-visualization",
+                "value": 193
+            },
+            {
+                "name": "rweekly.org",
+                "value": 191
+            },
+            {
+                "name": "gramm",
+                "value": 187
+            },
+            {
+                "name": "Cryptocurrency-Analysis-Python",
+                "value": 183
+            },
+            {
+                "name": "dashboard",
+                "value": 176
+            },
+            {
+                "name": "corner.py",
+                "value": 173
+            },
+            {
+                "name": "sjPlot",
+                "value": 169
+            },
+            {
+                "name": "MATLAB-Online",
+                "value": 167
+            },
+            {
+                "name": "AnyChart",
+                "value": 167
+            },
+            {
+                "name": "kbn_network",
+                "value": 165
+            },
+            {
+                "name": "react-vis-force",
+                "value": 163
+            },
+            {
+                "name": "data-science-on-gcp",
+                "value": 163
+            },
+            {
+                "name": "awesome-business-intelligence",
+                "value": 159
+            },
+            {
+                "name": "Treeverse",
+                "value": 156
+            },
+            {
+                "name": "IDACyber",
+                "value": 154
+            },
+            {
+                "name": "deepgraph",
+                "value": 145
+            },
+            {
+                "name": "iheatmapr",
+                "value": 142
+            },
+            {
+                "name": "react-plotly.js",
+                "value": 142
+            },
+            {
+                "name": "3d-force-graph",
+                "value": 139
+            },
+            {
+                "name": "tinychart",
+                "value": 137
+            },
+            {
+                "name": "hyperspy",
+                "value": 133
+            },
+            {
+                "name": "slopegraph",
+                "value": 122
+            },
+            {
+                "name": "orama",
+                "value": 120
+            },
+            {
+                "name": "vegalite",
+                "value": 119
+            },
+            {
+                "name": "react-jsx-highcharts",
+                "value": 116
+            },
+            {
+                "name": "leaflet.extras",
+                "value": 115
+            },
+            {
+                "name": "cookbook-2nd",
+                "value": 110
+            },
+            {
+                "name": "yt",
+                "value": 109
+            },
+            {
+                "name": "ggsci",
+                "value": 108
+            },
+            {
+                "name": "streamgraph",
+                "value": 106
+            },
+            {
+                "name": "OpenSpace",
+                "value": 101
+            },
+            {
+                "name": "intermine",
+                "value": 100
+            }
+        ]
+    }]
+};
